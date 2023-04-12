0726d6eaf368a22561f05202051f312935160676
Riley Betita <31970853+rileycbetita@users.noreply.github.com>
2023-04-12 11:41:29 -0700

    Treadmill and Background Manager Scripts Started
    Background Manager on Level gameobject, calls prefabs
    
    Treadmill handles the velocity of prefabs.
    
    To do: adjust prefab origins to correct default transformations. The goal is to move along the X-Axis

---
+++ Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour {
+    public Vector3 leftMovement;
+    public Vector3 leftThreshold;
+    public Vector3 rightThreshold;
+    public GameObject[] backgroundPrefabs;
+
+}
