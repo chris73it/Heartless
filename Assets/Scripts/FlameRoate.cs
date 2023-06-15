abc2dd3d93bba18b9017972ad2a4ceecb3755bdf
tblight1 <blighttimothy@gmail.com>
2023-06-15 11:43:57 -0700

    Restart button fixed, pitchforks and torches implemented

--- Assets/Scripts/FlameRoate.cs
+++ Assets/Scripts/FlameRoate.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class FlameRoate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public float rotationSpeed = 500f;
+        public float spinSpeed = 1000f;
+    
+
+   
     void Update()
     {
         
