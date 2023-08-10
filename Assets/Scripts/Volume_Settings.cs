5368ed32ad9d47ada9129517ff898d4ad448c60c
GVanniB <gvannibalenbin13@gmail.com>
2023-08-10 11:28:13 -0700

    Revert "Merge branch 'main' of https://github.com/chris73it/Heartless"
    This reverts commit 7d3a166379ecf6ec0a5f9ea843bed2b33d6f4f5d, reversing
    changes made to ca4bd68c1e1e3f20088faec573bbc803e8158d65.

--- Assets/Scripts/Volume_Settings.cs
+++ Assets/Scripts/Volume_Settings.cs
@@ -9,28 +9,20 @@ public class Volume_Settings : MonoBehaviour
     [SerializeField] private Slider SFXSlider;
     [SerializeField] private Slider musicSlider;
 
-    private float muteVolume = 0.0001f;
 
-    private string volumePrefKey = "VolumeSetting";
-    private string sfxPrefKey = "SFXSetting";
-    private string musicPrefKey = "MusicSetting";
+    private float muteVolume = 0.0001f; // Adjust this value as needed for near-mute behavior.
 
     private void Start()
     {
-        LoadVolumeSettings();
-    }
+        // Set the default volume here.
+        float defaultVolume = 0.5f; // You can set any value between 0.0f and 1.0f as the default.
+        volumeSlider.value = defaultVolume; // Set the slider value to the default.
+        SFXSlider.value = defaultVolume; // Set the slider value to the default.
+        musicSlider.value = defaultVolume; // Set the slider value to the default.
 
-    private void LoadVolumeSettings()
-    {
-        float defaultVolume = PlayerPrefs.GetFloat(volumePrefKey, 0.5f);
-        float defaultSFXVolume = PlayerPrefs.GetFloat(sfxPrefKey, 0.5f);
-        float defaultMusicVolume = PlayerPrefs.GetFloat(musicPrefKey, 0.5f);
 
-        volumeSlider.value = defaultVolume;
-        SFXSlider.value = defaultSFXVolume;
-        musicSlider.value = defaultMusicVolume;
 
-        SetVolume();
+        SetVolume(); // Call the SetVolume method to apply the default volume.
         SetSFXVolume();
         SetMusicVolume();
     }
@@ -38,33 +30,46 @@ public class Volume_Settings : MonoBehaviour
     public void SetVolume()
     {
         float volume = volumeSlider.value;
-        PlayerPrefs.SetFloat(volumePrefKey, volume);
-        ApplyVolume(volume, "Volume");
-    }
 
-    public void SetSFXVolume()
-    {
-        float volume = SFXSlider.value;
-        PlayerPrefs.SetFloat(sfxPrefKey, volume);
-        ApplyVolume(volume, "SFX");
-    }
-
-    public void SetMusicVolume()
-    {
-        float volume = musicSlider.value;
-        PlayerPrefs.SetFloat(musicPrefKey, volume);
-        ApplyVolume(volume, "Music");
-    }
-
-    private void ApplyVolume(float volume, string mixerParam)
-    {
         if (volume <= 0)
         {
-            myMixer.SetFloat(mixerParam, Mathf.Log10(muteVolume) * 20);
+            // If the volume is at or below 0, set it to muteVolume (almost mute).
+            myMixer.SetFloat("Volume", Mathf.Log10(muteVolume) * 20);
         }
         else
         {
-            myMixer.SetFloat(mixerParam, Mathf.Log10(volume) * 20);
+            // Otherwise, set the volume normally.
+            myMixer.SetFloat("Volume", Mathf.Log10(volume) * 20);
+        }
+    }
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+
+        if (volume <= 0)
+        {
+            // If the volume is at or below 0, set it to muteVolume (almost mute).
+            myMixer.SetFloat("SFX", Mathf.Log10(muteVolume) * 20);
+        }
+        else
+        {
+            // Otherwise, set the volume normally.
+            myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        }
+    }
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+
+        if (volume <= 0)
+        {
+            // If the volume is at or below 0, set it to muteVolume (almost mute).
+            myMixer.SetFloat("Music", Mathf.Log10(muteVolume) * 20);
+        }
+        else
+        {
+            // Otherwise, set the volume normally.
+            myMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
         }
     }
 }
