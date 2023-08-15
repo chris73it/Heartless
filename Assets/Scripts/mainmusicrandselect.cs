0f4a688e85aa4304dab1d96b9c35ef9a658ac6ad
Avanti-Stupid <123015022+Avanti-Stupid@users.noreply.github.com>
2023-08-15 12:45:52 -0700

    randomized game music

---
+++ Assets/Scripts/mainmusicrandselect.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainmusicrandselect : MonoBehaviour
+{
+  public  GameObject track1;
+    public GameObject track2;
+    public GameObject track3;
+   
+    void Start()
+    {
+       int goobertrack = Random.Range(1, 4);
+        //Debug.Log(goobertrack);
+
+        if (goobertrack == 1)
+        {
+            track1.SetActive(true);
+        }
+        else if (goobertrack == 2)
+        {
+            track2.SetActive(true);
+        }
+        else if (goobertrack == 3)
+        {
+            track3.SetActive(true);
+        }
+    }
+}
